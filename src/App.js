06fd2adf15d4162ede311ddf5ba1aa52a497b6c5
Rochelle Li <rochelleli165@gmail.com>
2026-04-17 13:47:11 -0500

    Add notion key

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ReactFlow, { Background, Controls } from 'reactflow';
 
 export default function App() {
